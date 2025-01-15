134151c5ae0f915c18ca7d582a12627f1ba29023
KrispiDev <krispidev@outlook.com>
2025-01-15 18:56:07 +0100

    💄 [DELETE]Se actualiza

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "reference_registry_url" {
-  value = "reference.gcr.io"
-}
-
-output "instance_registry_url" {
-  value = "instance.gcr.io"
-}
--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  description = "GCP Project ID"
-  default     = "shining-relic-447714-i8"
-}
-
-variable "region" {
-  description = "GCP Region"
-  default     = "us-central1"
-}
